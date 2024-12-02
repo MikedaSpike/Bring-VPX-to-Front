e9ab567ae25649e83ab5ac1f9194aa36f4e429da
Mike <48748234+MikedaSpike@users.noreply.github.com>
2024-12-02 07:03:48 +0100

    Added extra info

--- BringVPXtoFront/Form1.vb
+++ BringVPXtoFront/Form1.vb
@@ -1,17 +1,6 @@
 ﻿Imports System.ComponentModel
 Imports System.Management
-Imports System.Runtime.CompilerServices
 Imports System.Runtime.InteropServices
-Imports System.Security.Cryptography
-
-
-'Module ProcessExtensions
-'    <Extension()>
-'    Function GetChildProcesses(ByVal process As Process) As IList(Of Process)
-'        Return New ManagementObjectSearcher($"Select * From Win32_Process Where ParentProcessID={process.Id}").[Get]().Cast(Of ManagementObject)().[Select](Function(mo) Process.GetProcessById(Convert.ToInt32(mo("ProcessID")))).ToList()
-'    End Function
-'End Module
-
 
 Public Class Form1
     Private Shared ReadOnly synchLock As Object = New Object()
@@ -20,6 +9,11 @@ Public Class Form1
     Dim bVisualpinball = False
     Dim WithEvents ProcessStartWatcher As New ManagementEventWatcher(New WqlEventQuery("SELECT * FROM __InstanceCreationEvent WITHIN " & PollingInterval & " WHERE TargetInstance ISA 'Win32_Process'"))
     Dim WithEvents ProcessStopWatcher As New ManagementEventWatcher(New WqlEventQuery("SELECT * FROM __InstanceDeletionEvent WITHIN " & PollingInterval & " WHERE TargetInstance ISA 'Win32_Process'"))
+
+#Region "Form Events"
+    ''' <summary>
+    ''' Initializes the form on load.
+    ''' </summary>
     Private Sub Form1_Load(sender As Object, e As System.EventArgs) Handles MyBase.Load
         Me.Text = ProgramName
         UpdateLog("BVPTF (" & ProgramName & ") Version :" & Application.ProductVersion, Color.Blue)
@@ -30,11 +24,19 @@ Public Class Form1
         ProcessStopWatcher.Start()
     End Sub
 
+    ''' <summary>
+    ''' Handles form closing event.
+    ''' </summary>
     Private Sub Form1_FormClosing(sender As Object, e As System.Windows.Forms.FormClosingEventArgs)
         ProcessStartWatcher.Stop()
         ProcessStopWatcher.Stop()
     End Sub
+#End Region
 
+#Region "Process Watchers"
+    ''' <summary>
+    ''' Handles the start of processes.
+    ''' </summary>
     Private Sub ProcessStartWatcher_EventArrived(sender As Object, e As System.Management.EventArrivedEventArgs) Handles ProcessStartWatcher.EventArrived
         Dim ProcessName As String = CType(e.NewEvent.Properties("TargetInstance").Value, ManagementBaseObject)("Name")
         Dim PID As Integer = CType(e.NewEvent.Properties("TargetInstance").Value, ManagementBaseObject)("ProcessId")
@@ -42,19 +44,18 @@ Public Class Form1
         Select Case ProcessName.ToLower
             Case "VPinballX.exe".ToLower, "VPinballX64.exe".ToLower, "VPinballX_GL.exe".ToLower, "VPinballX_GL64.exe".ToLower, "VPinballX62.exe".ToLower, "VPinball995.exe".ToLower, "VPinball921.exe".ToLower, "VPinball99_PhysMod5_Updated.exe".ToLower, "VPinball8.exe".ToLower
                 UpdateLog(String.Format("Found process start of ""{0}"" with ID {1}.", ProcessName, PID))
-                'Application.DoEvents()
                 bVisualpinball = True
                 Delay(3000).ContinueWith(Sub() ActivateWindow(ProcessName, PID, "Visual Pinball Player"))
             Case "VPinballX_GL.exe".ToLower, "VPinballX_GL64.exe".ToLower
                 UpdateLog(String.Format("Found process start of ""{0}"" with ID {1}.", ProcessName, PID))
-                'Application.DoEvents()
                 bVisualpinball = True
                 Delay(3000).ContinueWith(Sub() ActivateWindow(ProcessName, PID, "Visual Pinball Player SDL"))
-
         End Select
-
     End Sub
 
+    ''' <summary>
+    ''' Handles the stop of processes.
+    ''' </summary>
     Private Sub ProcessStopWatcher_EventArrived(sender As Object, e As System.Management.EventArrivedEventArgs) Handles ProcessStopWatcher.EventArrived
         Dim ProcessName As String = CType(e.NewEvent.Properties("TargetInstance").Value, ManagementBaseObject)("Name")
         Dim PID As Integer = CType(e.NewEvent.Properties("TargetInstance").Value, ManagementBaseObject)("ProcessId")
@@ -62,75 +63,28 @@ Public Class Form1
             Case "VPinballX.exe".ToLower, "VPinballX_GL.exe".ToLower, "VPinballX64.exe".ToLower, "VPinballX_GL64.exe".ToLower
                 bVisualpinball = False
         End Select
-        'MessageBox.Show(String.Format("Process ""{0}"" with ID {1} stopped.", ProcessName, PID))
     End Sub
+#End Region
 
+#Region "Utility Functions"
+    ''' <summary>
+    ''' Creates an asynchronous delay.
+    ''' </summary>
     Public Shared Function Delay(ByVal milliseconds As Double) As Threading.Tasks.Task
-        'asynchronous delay. 
         Dim tcs = New Threading.Tasks.TaskCompletionSource(Of Boolean)()
         Dim timer As System.Timers.Timer = New System.Timers.Timer()
         AddHandler timer.Elapsed, Sub(obj, args)
                                       tcs.TrySetResult(True)
                                   End Sub
-
         timer.Interval = milliseconds
         timer.AutoReset = False
         timer.Start()
         Return tcs.Task
     End Function
 
-
-    'Private Sub ActivateWindow(ProcessName As String, PID As Integer, Optional WindowToactivate As String = "")
-    '    Try
-    '        Debug.WriteLine("ActivateWindow [" & ProcessName, "] PID [" & PID & "] WindowToactivate [" & WindowToactivate & "]")
-    '        If WindowToactivate = "" Then
-    '            Dim p As Process
-    '            Dim window_name As String = Process.GetProcessById(PID).MainWindowTitle.ToString
-    '            AppActivate(window_name)
-    '            For Each p In Process.GetProcessesByName(PID)
-    '                window_name = p.MainWindowTitle.ToString
-    '                AppActivate(window_name)
-    '                UpdateLog("Activated window name [" & window_name & "]")
-    '            Next
-    '        Else
-    '            Dim stopwatch As Stopwatch = New Stopwatch()
-    '            stopwatch.Start()
-    '            Dim stopDoWhile As Boolean = False
-    '            'Need specific window to activate
-    '            Do While Process.GetProcessById(PID).MainWindowTitle.ToString.ToLower <> WindowToactivate.ToLower And stopDoWhile = False
-    '                Threading.Thread.Sleep(500)
-    '                If stopwatch.ElapsedMilliseconds > 15000 Then stopDoWhile = True
-    '                Application.DoEvents()
-    '            Loop
-    '            stopwatch.Stop()
-    '            'Threading.Thread.Sleep(1000)
-    '            Debug.WriteLine("Active window " & ProcessHelper.GetActiveProcess().MainWindowTitle.ToString)
-    '            If stopDoWhile = False Then
-    '                AppActivate(WindowToactivate)
-    '                UpdateLog("brought to front : [" & WindowToactivate & "]")
-    '                'start timer for 30 seconde
-    '                stopwatch.Start()
-    '                Do While stopwatch.ElapsedMilliseconds < 30000
-    '                    If ProcessHelper.GetActiveProcess().MainWindowTitle.ToString.ToLower <> WindowToactivate.ToLower Then
-    '                        AppActivate(WindowToactivate)
-    '                        UpdateLog("Brought to front again : [" & WindowToactivate & "]", Color.OrangeRed)
-    '                        Threading.Thread.Sleep(500)
-    '                    End If
-    '                Loop
-    '            Else
-    '                UpdateLog("Timeout to find window [" & WindowToactivate & "]", Color.Red)
-    '                Dim p As Process
-    '                For Each p In GetChildProcesses(Process.GetProcessById(PID))
-    '                    UpdateLog("found process: [" & p.MainWindowTitle.ToString & "]")
-    '                Next
-
-    '            End If
-    '        End If
-    '    Catch ex As Exception
-    '        LogAnError(System.Reflection.MethodBase.GetCurrentMethod.Name, ex.Message)
-    '    End Try
-    'End Sub
-
+    ''' <summary>
+    ''' Activates a window by its title.
+    ''' </summary>
     Private Sub ActivateWindow(ProcessName As String, PID As Integer, WindowToactivate As String)
         Try
             Dim stopwatch As Stopwatch = New Stopwatch()
@@ -138,23 +92,18 @@ Public Class Form1
             Dim exitDoWhile As Boolean = False
             UpdateLog("Waiting for window: [" & WindowToactivate & "] for 60 seconds")
             stopwatch.Start()
-            'Need specific window to activate
             Do While exitDoWhile = False AndAlso WindowFound = False AndAlso bVisualpinball = True
                 Try
                     AppActivate(WindowToactivate)
                     UpdateLog("Window [" & WindowToactivate & "] found and set to front")
                     WindowFound = True
                 Catch ex As Exception
-
                 End Try
-
                 Threading.Thread.Sleep(500)
                 If stopwatch.ElapsedMilliseconds > 60000 Then exitDoWhile = True
             Loop
             stopwatch.Stop()
             stopwatch.Reset()
-            'Threading.Thread.Sleep(1000)
-            Debug.WriteLine("Active window " & ProcessHelper.GetActiveProcess().MainWindowTitle.ToString)
             If exitDoWhile = False Then
                 UpdateLog("Monitoring window: [" & WindowToactivate & "] for 30 seconds and bring to front")
                 stopwatch.Start()
@@ -162,15 +111,11 @@ Public Class Form1
                     If bVisualpinball = False Then UpdateLog("Process : [" & ProcessName & "] stopped. Not monitoring anymore") : Exit Sub
                     If ProcessHelper.GetActiveProcess().MainWindowTitle.ToString.ToLower <> WindowToactivate.ToLower Then
                         Try
-                            'updateLog("Active window " & ProcessHelper.GetActiveProcess().MainWindowTitle.ToString)
                             AppActivate(WindowToactivate)
-                            'UpdateLog("Brought to front again : [" & WindowToactivate & "]", Color.OrangeRed)
-
                         Catch ex As Exception
                             UpdateLog("Not found : [" & WindowToactivate & "]. Stop monitoring", Color.Red)
                             Exit Sub
                         End Try
-
                         Threading.Thread.Sleep(500)
                     End If
                 Loop
@@ -179,12 +124,16 @@ Public Class Form1
             Else
                 UpdateLog("Timeout to find window [" & WindowToactivate & "]", Color.Red)
             End If
-
         Catch ex As Exception
             LogAnError(System.Reflection.MethodBase.GetCurrentMethod.Name, ex.Message)
         End Try
     End Sub
+#End Region
 
+#Region "Form Behavior"
+    ''' <summary>
+    ''' Handles form resize events.
+    ''' </summary>
     Private Sub Form1_Resize(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Resize
         If Me.WindowState = FormWindowState.Minimized Then
             Me.ShowInTaskbar = False
@@ -193,35 +142,41 @@ Public Class Form1
             Me.Hide()
             TrayIcon.BalloonTipIcon = ToolTipIcon.Info
             TrayIcon.Text = "Open '" & ProgramName & "' log file"
-            'TrayIcon.BalloonTipText = "Open 'Bring Visual Pinball To Top' log file"
-            'TrayIcon.BalloonTipTitle = "BVPTT"
-            'TrayIcon.ShowBalloonTip(500)
-
         End If
     End Sub
+
+    ''' <summary>
+    ''' Handles form closing.
+    ''' </summary>
     Private Sub Form1_Closing(sender As Object, e As CancelEventArgs) Handles MyBase.Closing
         TrayIcon.Visible = False
         Me.ShowInTaskbar = False
     End Sub
 
+    ''' <summary>
+    ''' Handles system tray icon double-click.
+    ''' </summary>
     Private Sub NotifyIcon1_DoubleClick(ByVal sender As Object, ByVal e As System.EventArgs) Handles TrayIcon.DoubleClick
         Me.Show()
         Me.WindowState = FormWindowState.Normal
         TrayIcon.Visible = False
     End Sub
 
+    ''' <summary>
+    ''' Handles system tray icon click.
+    ''' </summary>
     Private Sub NotifyIcon1_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles TrayIcon.Click
         Me.Show()
         Me.WindowState = FormWindowState.Normal
         TrayIcon.Visible = False
         Me.ShowInTaskbar = True
     End Sub
+#End Region
 
-
-    '*********************************************************************************************************
-    ' Function : LogAnError
-    ' Purpose  : Logs an error to logfile, optionally also adds so action log window
-    '*********************************************************************************************************
+#Region "Logging"
+    ''' <summary>
+    ''' Logs errors.
+    ''' </summary>
     Public Sub LogAnError(ByVal ErrorSubName As String, ErrorMessage As String)
         Try
             UpdateLog(ErrorSubName & vbTab & ErrorMessage, Color.Red)
@@ -229,14 +184,12 @@ Public Class Form1
         End Try
     End Sub
 
-    '*********************************************************************************************************
-    ' Function : UpdateActionLog
-    ' Purpose  : Add text to the action window
-    '*********************************************************************************************************
+    ''' <summary>
+    ''' Logs actions.
+    ''' </summary>
     Private Sub UpdateLog(ActionText As String, Optional Textcolor As Color = Nothing)
         Try
             Dim unused = BeginInvoke(New Action(Function()
-
                                                     SyncLock synchLock
                                                         If Textcolor = Nothing Then
                                                             rtbActionsLog.SelectionColor = Color.Black
@@ -247,29 +200,32 @@ Public Class Form1
                                                     End SyncLock
                                                 End Function))
         Catch ex As Exception
-            'LogAnError(System.Reflection.MethodBase.GetCurrentMethod.Name, ex.Message)
-
         End Try
     End Sub
-
+#End Region
 End Class
-
-
-
-
 
 Public NotInheritable Class ProcessHelper
     Private Sub New() 'Make no instances of this class.
     End Sub
 
+    ''' <summary>
+    ''' Retrieves the handle of the foreground window.
+    ''' </summary>
     <DllImport("user32.dll", SetLastError:=True)>
     Private Shared Function GetForegroundWindow() As IntPtr
     End Function
 
+    ''' <summary>
+    ''' Retrieves the process ID of the specified window.
+    ''' </summary>
     <DllImport("user32.dll", SetLastError:=True)>
     Private Shared Function GetWindowThreadProcessId(ByVal hWnd As IntPtr, ByRef lpdwProcessId As UInteger) As Integer
     End Function
 
+    ''' <summary>
+    ''' Gets the process associated with the current foreground window.
+    ''' </summary>
     Public Shared Function GetActiveProcess() As Process
         Dim FocusedWindow As IntPtr = GetForegroundWindow()
         If FocusedWindow = IntPtr.Zero Then Return Nothing
